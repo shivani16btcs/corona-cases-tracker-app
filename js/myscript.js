41c372b5cc0db1194acd12e4a9a9992dfcf25d90
Shekhar Garg <shekhar.garg@daffodilsw.com>
2020-09-16 15:09:31 +0530

    docter review

--- js/myscript.js
+++ js/myscript.js
@@ -1,49 +1,19 @@
-const Url = "https://covid19.mathdro.id/api";
+//const Url = "https://covid19.mathdro.id/api";
+//const Url="https://healthcare.googleapis.com/$discovery/rest?version=v1";
+const Url="https://mybusiness.googleapis.com/v4/accounts/docterreview/locations/1047892681669/reviews"
 let app = angular.module("MyApp", []);
 
 app.controller("MyCtrl", ($scope, $http) => {
   //this is controller
-  $scope.title = "stay home stay safe ";
+  $scope.title = "DOCTOR reviews ";
   $http.get(Url).then(
     (response) => {
         console.log(response.data)
-        $scope.alldata=response.data;
+        $scope.alldata=JSON.stringify(response.data);
 
     },
     (error) => {
         console.log(error);
     }
   );
-
-  $scope.get_c_data=async()=>{
-      let country=$scope.c;
-      if(country==' '){
-          $scope.c_data=undefined;
-          return
-      }
-      await $http.get(`${Url}/countries/`).then(
-          (response)=>{
-              $scope.c_data=response.data.countries;
-              console.log($scope.c_data);
-          },
-          (error)=>{
-              console.log(error);
-          }
-      )
-  }
-
-
-  $scope.detail=async()=>{
-
-   await   console.log("hello");
-    
-  }
-  
-});
-
-
-
-$(function() {
-    $('#table').bootstrapTable()
-  })
-
+ });
